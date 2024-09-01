89d8b4a7cdd479c0c42214e5ab6f7b6892df9f74
ds5678 <49847914+ds5678@users.noreply.github.com>
2024-09-01 08:15:23 -0700

    Event and property extensions

---
+++ AssetRipper.CIL/EventDefinitionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using AsmResolver.DotNet;
+
+namespace AssetRipper.CIL;
+
+public static class EventDefinitionExtensions
+{
+	public static bool IsInstance(this EventDefinition eventDefinition)
+	{
+		return (eventDefinition.AddMethod?.IsInstance() ?? false)
+			|| (eventDefinition.RemoveMethod?.IsInstance() ?? false);
+	}
+
+	public static bool IsStatic(this EventDefinition eventDefinition)
+	{
+		return (eventDefinition.AddMethod?.IsStatic ?? false)
+			|| (eventDefinition.RemoveMethod?.IsStatic ?? false);
+	}
+
+	public static bool HasAddMethod(this EventDefinition eventDefinition)
+	{
+		return eventDefinition.AddMethod is not null;
+	}
+
+	public static bool HasRemoveMethod(this EventDefinition eventDefinition)
+	{
+		return eventDefinition.RemoveMethod is not null;
+	}
+
+	public static bool HasFireMethod(this EventDefinition eventDefinition)
+	{
+		return eventDefinition.FireMethod is not null;
+	}
+}
---
+++ AssetRipper.CIL/PropertyDefinitionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using AsmResolver.DotNet;
+using System.Diagnostics.CodeAnalysis;
+
+namespace AssetRipper.CIL;
+
+public static class PropertyDefinitionExtensions
+{
+	public static bool IsAutoProperty(this PropertyDefinition property, [NotNullWhen(true)] out FieldDefinition? backingField)
+	{
+		backingField = null;
+		return (property.GetMethod is { } getMethod && getMethod.IsAutoPropertyGetMethod(out backingField))
+			|| (property.SetMethod is { } setMethod && setMethod.IsAutoPropertySetMethod(out backingField));
+	}
+
+	public static bool HasGetMethod(this PropertyDefinition property)
+	{
+		return property.GetMethod is not null;
+	}
+
+	public static bool HasSetMethod(this PropertyDefinition property)
+	{
+		return property.SetMethod is not null;
+	}
+
+	public static bool HasBothAccessors(this PropertyDefinition property)
+	{
+		return property.HasGetMethod() && property.HasSetMethod();
+	}
+
+	public static bool IsInstance(this PropertyDefinition property) => property.Signature?.HasThis ?? false;
+
+	public static bool IsStatic(this PropertyDefinition property) => !property.Signature?.HasThis ?? false;
+}
