1df36de7df0be76bb78c4a370f3249fe52933364
ds5678 <49847914+ds5678@users.noreply.github.com>
2024-09-01 08:21:39 -0700

    Field extensions

---
+++ AssetRipper.CIL/FieldDefinitionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using AsmResolver.DotNet;
+
+namespace AssetRipper.CIL;
+
+public static class FieldDefinitionExtensions
+{
+	public static bool IsInstance(this FieldDefinition field)
+	{
+		return !field.IsStatic;
+	}
+}
