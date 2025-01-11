1338ca2f950a93fb6b8ba3d4ebc32a498190cbf7
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-01-10 23:46:05 -0800

    Add method stubbing tool

---
+++ AssetRipper.CIL.Stubber/Program.cs
@@ -0,0 +1,18 @@
+﻿using AsmResolver.DotNet;
+
+namespace AssetRipper.CIL.Stubber
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			ModuleDefinition module = ModuleDefinition.FromFile(args[0]);
+			foreach (MethodDefinition method in module.GetAllTypes().SelectMany(t => t.Methods))
+			{
+				method.ReplaceMethodBodyWithMinimalImplementation();
+			}
+			module.Write(args[1]);
+			Console.WriteLine("Done!");
+		}
+	}
+}
